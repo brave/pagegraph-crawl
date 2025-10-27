bdfcb733d76cc443a7e0b4018813bfe205141d9a
mschfh <37435502+mschfh@users.noreply.github.com>
2025-10-27 14:06:51 -0500

    fix: implement setuid sandbox option

--- src/brave/puppeteer.ts
+++ src/brave/puppeteer.ts
@@ -106,6 +106,11 @@ const makePuppeteerConf = async (args: CrawlArgs): Promise<PuppeteerConfig> => {
     '--user-data-dir=' + profilePath,
   ]
 
+  // Add --disable-setuid-sandbox if environment variable is set
+  if (process.env.PAGEGRAPH_DISABLE_SETUID_SANDBOX === 'true') {
+    chromeArgs.push('--disable-setuid-sandbox')
+  }
+
   const puppeteerArgs = {
     defaultViewport: null,
     args: chromeArgs,
